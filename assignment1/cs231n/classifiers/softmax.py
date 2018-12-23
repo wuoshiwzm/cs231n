409e8f5dc5a996c068ddb21b66060cd1577479a5
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2018-12-23 11:35:47 +0800

    12-23

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -1,117 +1,164 @@
+# coding=UTF-8
 import numpy as np
 from random import shuffle
 
 
 def softmax_loss_naive(W, X, y, reg):
-  """
-  Softmax loss function, naive implementation (with loops)
+    """
+    Softmax loss function, naive implementation (with loops)
 
-  Inputs have dimension D, there are C classes, and we operate on minibatches
-  of N examples.
+    Inputs have dimension D, there are C classes, and we operate on minibatches
+    of N examples.
 
-  Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
-  - y: A numpy array of shape (N,) containing training labels; y[i] = c means
-    that X[i] has label c, where 0 <= c < C.
-  - reg: (float) regularization strength
+    Inputs:
+    - W: A numpy array of shape (D, C) containing weights.
+    - X: A numpy array of shape (N, D) containing a minibatch of data.
+    - y: A numpy array of shape (N,) containing training labels; y[i] = c means
+      that X[i] has label c, where 0 <= c < C.
+    - reg: (float) regularization strength
 
-  Returns a tuple of:
-  - loss as single float
-  - gradient with respect to weights W; an array of same shape as W
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Returns a tuple of:
+    - loss as single float
+    - gradient with respect to weights W; an array of same shape as W
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
+    # 生成W一样shape的全0矩阵
+    dW = np.zeros_like(W)
 
-  scores = np.dot(X, W)
-  e_scores = np.exp(scores)
-  e_scores_sum = np.sum(e_scores, axis=1)
-  e_scores_yi = e_scores[range(scores.shape[0]), y]
-  L_i = - np.log(e_scores_yi / e_scores_sum)
-  L = np.mean(L_i)
-  loss = L + 0.5 * reg * np.sum(W * W)
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
+    # 得分
+    scores = np.dot(X, W)
+    # 转换成指数
+    exp_scores = np.exp(scores)
+    exp_scores_sum = np.sum(exp_scores, axis=1)
+    # 标签值
+    exp_scores_y = exp_scores[range(scores.shape[0]), y]
 
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
+    # 求总和
+    exp_sum = np.sum(exp_scores)
+    # 归一化
+    normalize_scores = exp_scores_y / exp_scores_sum
+    Li = -np.log(normalize_scores)
 
-  return loss, dW
+    # 对Li 求平均
+    Li_mean = np.mean(Li)
+    loss = Li_mean + 0.5 * reg * np.sum(W * W)
+
+    #
+    # scores = np.dot(X, W)
+    # e_scores = np.exp(scores)
+    # e_scores_sum = np.sum(e_scores, axis=1)
+    # # e_scores[range(500),y]
+    # # e_scores 对应的标签
+    #
+    # e_scores_yi = e_scores[range(scores.shape[0]), y]
+    # L_i = - np.log(e_scores_yi / e_scores_sum)
+    #
+    # L = np.mean(L_i)
+    # loss = L + 0.5 * reg * np.sum(W * W)
+    #
+
+
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
 
 
 def softmax_loss_vectorized(W, X, y, reg):
-  """
-  Softmax loss function, vectorized version.
+    """
+    Softmax loss function, vectorized version.
 
-  Inputs and outputs are the same as softmax_loss_naive.
-  """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+    Inputs and outputs are the same as softmax_loss_naive.
+    """
+    # Initialize the loss and gradient to zero.
+    loss = 0.0
+    dW = np.zeros_like(W)
 
-  #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
-  # Store the loss in loss and the gradient in dW. If you are not careful     #
-  # here, it is easy to run into numeric instability. Don't forget the        #
-  # regularization!                                                           #
-  #############################################################################
-  W = W.T
-  X = X.T
+    #############################################################################
+    # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+    # Store the loss in loss and the gradient in dW. If you are not careful     #
+    # here, it is easy to run into numeric instability. Don't forget the        #
+    # regularization!                                                           #
+    #############################################################################
 
-  (C, D) = W.shape
-  N = X.shape[1]
 
-  scores = np.dot(W, X)
-  scores -= np.max(scores) # shift by log C to avoid numerical instability
 
-  y_mat = np.zeros(shape = (C, N))
-  y_mat[y, range(N)] = 1
 
-  # matrix of all zeros except for a single wx + log C value in each column that corresponds to the
-  # quantity we need to subtract from each row of scores
-  correct_wx = np.multiply(y_mat, scores)
 
-  # create a single row of the correct wx_y + log C values for each data point
-  sums_wy = np.sum(correct_wx, axis=0) # sum over each column
 
-  exp_scores = np.exp(scores)
-  sums_exp = np.sum(exp_scores, axis=0) # sum over each column
-  result = np.log(sums_exp)
 
-  result -= sums_wy
 
-  loss = np.sum(result)
 
-  # Right now the loss is a sum over all training examples, but we want it
-  # to be an average instead so we divide by num_train.
-  loss /= float(N)
 
-  # Add regularization to the loss.
-  loss += 0.5 * reg * np.sum(W * W)
 
-  sum_exp_scores = np.sum(exp_scores, axis=0) # sum over columns
-  sum_exp_scores = 1.0 / (sum_exp_scores + 1e-8)
 
-  dW = exp_scores * sum_exp_scores
-  dW = np.dot(dW, X.T)
-  dW -= np.dot(y_mat, X.T)
 
-  dW /= float(N)
 
-  # Add regularization to the gradient
-  dW += reg * W
 
-  dW = dW.T
-  #############################################################################
-  #                          END OF YOUR CODE                                 #
-  #############################################################################
 
-  return loss, dW
 
+
+
+
+
+    W = W.T
+    X = X.T
+
+    (C, D) = W.shape
+    N = X.shape[1]
+
+    scores = np.dot(W, X)
+    scores -= np.max(scores)  # shift by log C to avoid numerical instability
+
+    y_mat = np.zeros(shape=(C, N))
+    y_mat[y, range(N)] = 1
+
+    # matrix of all zeros except for a single wx + log C value in each column that corresponds to the
+    # quantity we need to subtract from each row of scores
+    correct_wx = np.multiply(y_mat, scores)
+
+    # create a single row of the correct wx_y + log C values for each data point
+    sums_wy = np.sum(correct_wx, axis=0)  # sum over each column
+
+    exp_scores = np.exp(scores)
+    sums_exp = np.sum(exp_scores, axis=0)  # sum over each column
+    result = np.log(sums_exp)
+
+    result -= sums_wy
+
+    loss = np.sum(result)
+
+    # Right now the loss is a sum over all training examples, but we want it
+    # to be an average instead so we divide by num_train.
+    loss /= float(N)
+
+    # Add regularization to the loss.
+    loss += 0.5 * reg * np.sum(W * W)
+
+    sum_exp_scores = np.sum(exp_scores, axis=0)  # sum over columns
+    sum_exp_scores = 1.0 / (sum_exp_scores + 1e-8)
+
+    dW = exp_scores * sum_exp_scores
+    dW = np.dot(dW, X.T)
+    dW -= np.dot(y_mat, X.T)
+
+    dW /= float(N)
+
+    # Add regularization to the gradient
+    dW += reg * W
+
+    dW = dW.T
+    #############################################################################
+    #                          END OF YOUR CODE                                 #
+    #############################################################################
+
+    return loss, dW
--- assignment1/cs231n/gradient_check.py
+++ assignment1/cs231n/gradient_check.py
@@ -106,7 +106,7 @@ def eval_numerical_gradient_net(net, inputs, output, h=1e-5):
 
 def grad_check_sparse(f, x, analytic_grad, num_checks=10, h=1e-5):
     """
-    解析梯度
+    根据定义求梯度
     sample a few random elements and only return numerical
     in this dimensions.
     """
